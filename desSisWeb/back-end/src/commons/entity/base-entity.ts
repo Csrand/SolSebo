900469fe60efb71f1ddcb29e44dcfccc3d25449e
Csrand <chrystiansenna@gmail.com>
2026-04-28 16:08:41 -0300

    chore: Entidade Base com id, padrao DRY.
    Essa alteração permite que todas as entidades terao ID's únicos, sem a
    necessidade de especificar o id, como fazimos com idUser

--- desSisWeb/back-end/src/commons/entity/base-entity.ts
+++ desSisWeb/back-end/src/commons/entity/base-entity.ts
@@ -1,6 +1,16 @@
-import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export abstract class BaseEntity {
+  @PrimaryGeneratedColumn('increment', {
+    name: 'ID_USUARIO',
+    type: 'int',
+  })
+  id: number;
+
   @CreateDateColumn({ name: 'CREATED_AT' })
   createdAt!: Date;
 
